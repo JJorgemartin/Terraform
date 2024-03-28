1752dea0f51c17d765e0e2e6ec09ca47eb5ae436
jjorgemartin <jorgemartin.jjm@gmail.com>
2024-03-28 17:56:27 -0600

    Add if-statement in a ASG schedule

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -41,6 +41,30 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  count = var.enable_autoscaling ? 1 : 0
+
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 10
+  recurrence            = "0 9 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  count = var.enable_autoscaling ? 1 : 0
+
+  scheduled_action_name = "scale-in-at-night"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 2
+  recurrence            = "0 17 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
 resource "aws_lb" "example" {
   name = "${var.cluster_name}-alb"
   load_balancer_type = "application"
--- modules/services/webserver-cluster/variables.tf
+++ modules/services/webserver-cluster/variables.tf
@@ -38,5 +38,9 @@ variable "custom_tags" {
   description = "Custom tags to set on the Instances in the ASG"
   type = map(string)
   default = {}
-  
+}
+
+variable "enable_autoscaling" {
+  description = "If set to true, enable auto scaling"
+  type = bool
 }
