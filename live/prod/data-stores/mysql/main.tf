b269ff22c392119ffe7a14d663fd306faf72d34c
jjorgemartin <jorgemartin.jjm@gmail.com>
2024-04-01 15:25:44 -0600

    changes in mysql folder

--- live/prod/data-stores/mysql/main.tf
+++ live/prod/data-stores/mysql/main.tf
@@ -1,31 +1,61 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  #terraform {
+  #  backend "s3" {
+  #	bucket = "jjterraform-up-and-running-state"
+  #	key = "live/prod/data-store/mysql/terraform.tfstate"
+  #	region = "us-east-2"
+  #	dynamodb_table = "terraform-up-and-running-locks"
+  #	encrypt = true
+  #  }
+  #}
+}
+
+
 provider "aws" {
   region = "us-east-2"
+  alias  = "primary"
 }
 
-#terraform {
-#  backend "s3" {
-#	bucket = "jjterraform-up-and-running-state"
-#	key = "live/prod/data-store/mysql/terraform.tfstate"
-#	region = "us-east-2"
-#	dynamodb_table = "terraform-up-and-running-locks"
-#	encrypt = true
-#  }
-#}
-
-resource "aws_db_instance" "example" {
-  identifier_prefix = "terraform-up-and-running"
-  engine = "mysql"
-  allocated_storage = 10
-  instance_class = "db.t2.micro"
-  skip_final_snapshot = true
-  db_name = var.db_name
-
-  #How should we set the username and password
-
-  username = local.db_creds.username
-  password = local.db_creds.password
-  
+provider "aws" {
+  region = "us-west-1"
+  alias  = "replica"
 }
+
+module "mysql_primary" {
+  source = "../../../../modules/data-stores/mysql"
+
+  providers = {
+    aws = aws.primary
+  }
+
+  db_name     = var.db_name
+
+  db_username = var.db_username
+  db_password = var.db_password
+
+  # Must be enabled to support replication
+  backup_retention_period = 1
+}
+
+module "mysql_replica" {
+  source = "../../../../modules/data-stores/mysql"
+
+  providers = {
+    aws = aws.replica
+  }
+
+  # Make this a replica of the primary
+  replicate_source_db = module.mysql_primary.arn
+}
+
 
 #data "aws_kms_secrets" "creds" {
 #  secret {
