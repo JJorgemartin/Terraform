0f07f9155fb2bbdce199203d7d53717fdf3c249b
jjorgemartin <jorgemartin.jjm@gmail.com>
2024-04-03 13:08:36 -0600

    terraform fmt application for visual enhancement

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -1,32 +1,32 @@
 resource "aws_launch_configuration" "example" {
-	image_id = var.ami
-	instance_type = var.instance_type
-	security_groups = [ aws_security_group.instance.id ]
-	
-  	user_data = templatefile("${path.module}/user-data.sh", {
-		server_port = var.server_port
-		db_address = data.terraform_remote_state.db.outputs.address
-		db_port = data.terraform_remote_state.db.outputs.port
+  image_id        = var.ami
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = templatefile("${path.module}/user-data.sh", {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
     server_text = var.server_text
-	})
-	
-	#Required when using a launch configuration with an auto scaling group
-	lifecycle {
-	  create_before_destroy = true
-	}
+  })
+
+  #Required when using a launch configuration with an auto scaling group
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "example" {
-  
+
   # Explicitly depend on the launch configuration's name os each time it'sreplaced, this ASG is also replaced 
   name = var.cluster_name
 
   launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnets.default.ids
-  target_group_arns = [ aws_lb_target_group.asg.arn ]
-  health_check_type = "ELB"
-  min_size = var.min_size
-  max_size = var.max_size
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+  target_group_arns    = [aws_lb_target_group.asg.arn]
+  health_check_type    = "ELB"
+  min_size             = var.min_size
+  max_size             = var.max_size
 
   # Use instance refresh to roll out changes to the ASG
   instance_refresh {
@@ -35,24 +35,24 @@ resource "aws_autoscaling_group" "example" {
       min_healthy_percentage = 50
     }
   }
-   
- 
+
+
   tag {
-	key = "Name"
-	value = var.cluster_name
-	propagate_at_launch = true
+    key                 = "Name"
+    value               = var.cluster_name
+    propagate_at_launch = true
   }
 
   dynamic "tag" {
     for_each = {
-      for key, value in var.custom_tags:
+      for key, value in var.custom_tags :
       key => upper(value)
       if key != "Name"
     }
-    
+
     content {
-      key = tag.key
-      value = tag.value
+      key                 = tag.key
+      value               = tag.value
       propagate_at_launch = true
     }
   }
@@ -83,25 +83,25 @@ resource "aws_autoscaling_schedule" "scale_in_at_night" {
 }
 
 resource "aws_lb" "example" {
-  name = var.cluster_name
+  name               = var.cluster_name
   load_balancer_type = "application"
-  subnets = data.aws_subnets.default.ids
-  security_groups = [ aws_security_group.alb.id ]
-}	
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port = local.http_port
-  protocol = "HTTP"
+  port              = local.http_port
+  protocol          = "HTTP"
 
   #By default, return a simple 404 page
   default_action {
-	type = "fixed-response"
-	fixed_response {
-	  content_type = "text/plain"
-	  message_body = "404 : page not found"
-	  status_code = 404
-	}
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404 : page not found"
+      status_code  = 404
+    }
   }
 }
 
@@ -124,22 +124,22 @@ resource "aws_security_group" "alb" {
 }
 
 resource "aws_security_group_rule" "allow_http_inbound" {
-  type = "ingress"
+  type              = "ingress"
   security_group_id = aws_security_group.alb.id
 
-  from_port = local.http_port
-  to_port = local.http_port
-  protocol = local.tcp_protocol
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
   cidr_blocks = local.all_ips
 }
 
 resource "aws_security_group_rule" "allow_all_outbound" {
-  type = "egress"
+  type              = "egress"
   security_group_id = aws_security_group.alb.id
-  
-  from_port = local.http_port
-  to_port = local.http_port
-  protocol = local.tcp_protocol
+
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
   cidr_blocks = local.all_ips
 }
 
@@ -149,38 +149,38 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_lb_target_group" "asg" {
-	name = var.cluster_name
-	port = var.server_port
-	protocol = "HTTP"
-	vpc_id = data.aws_vpc.default.id
-	health_check {
-		path = "/"
-		protocol = "HTTP"
-		matcher = "200"
-		interval = 15
-		timeout = 3
-		healthy_threshold = 2
-		unhealthy_threshold = 2   
-	}  
+  name     = var.cluster_name
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_listener_rule" "asg" {
   listener_arn = aws_lb_listener.http.arn
-  priority = 100
+  priority     = 100
   condition {
-	  path_pattern {
-	    values = ["*"]
-	  }
+    path_pattern {
+      values = ["*"]
+    }
   }
   action {
-	  type = "forward"
-	  target_group_arn = aws_lb_target_group.asg.arn
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
 
 resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
-  alarm_name = "${var.cluster_name}-high-cpu-utilization"
-  namespace = "AWS/EC2"
+  alarm_name  = "${var.cluster_name}-high-cpu-utilization"
+  namespace   = "AWS/EC2"
   metric_name = "CPUUtilization"
 
   dimensions = {
@@ -188,39 +188,39 @@ resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
   }
 
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = 1
-  period = 300
-  statistic = "Average"
-  threshold = 90
-  unit = "Percent"
+  evaluation_periods  = 1
+  period              = 300
+  statistic           = "Average"
+  threshold           = 90
+  unit                = "Percent"
 }
 
 
 resource "aws_cloudwatch_metric_alarm" "low_cpu_credit_balance" {
   count = format("%.1s", var.instance_type) == "t" ? 1 : 0
 
-  alarm_name = "${var.cluster_name}-low-cpu-credit-balance"
-  namespace = "AWS/EC2"
+  alarm_name  = "${var.cluster_name}-low-cpu-credit-balance"
+  namespace   = "AWS/EC2"
   metric_name = "CPUCreditBalance"
 
   dimensions = {
     AutoScalingGroupName = aws_autoscaling_group.example.name
   }
-  
+
   comparison_operator = "LessThanThreshold"
-  evaluation_periods = 1
-  period = 300
-  statistic = "Minimum"
-  threshold = 10
-  unit = "Count"
+  evaluation_periods  = 1
+  period              = 300
+  statistic           = "Minimum"
+  threshold           = 10
+  unit                = "Count"
 }
 
 
 data "aws_subnets" "default" {
-	filter {
-	  name = "vpc-id"
-	  values = [ data.aws_vpc.default.id ]
-	} 
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 #data "terraform_remote_state" "db" {
@@ -235,9 +235,9 @@ data "aws_subnets" "default" {
 
 
 locals {
-  http_port = 80
-  any_port = 0
+  http_port    = 80
+  any_port     = 0
   any_protocol = "-1"
   tcp_protocol = "tcp"
-  all_ips = ["0.0.0.0/0"]
+  all_ips      = ["0.0.0.0/0"]
 }
--- modules/services/webserver-cluster/variables.tf
+++ modules/services/webserver-cluster/variables.tf
@@ -1,58 +1,58 @@
 variable "server_port" {
-	description = "The port the server will use for HTTP requests"
-	type = number
-	default = 8080
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
 }
 
 variable "cluster_name" {
   description = "The name to use for all the cluster resources"
-  type = string
+  type        = string
 }
 
 variable "db_remote_state_bucket" {
   description = "The name of the s3 bucket for the database's remote state"
-  type = string
+  type        = string
 }
 
 variable "db_remote_state_key" {
   description = "The path for the database's remote state in s3"
-  type = string
+  type        = string
 }
 
 variable "instance_type" {
   description = "The type of EC2 Instances to run (e.g. t2.micro)"
-  type = string
+  type        = string
 }
 
 variable "min_size" {
   description = "The minium number of EC2 Instances in the ASG"
-  type = number
+  type        = number
 }
 
 variable "max_size" {
   description = "The maximum number of EC2 Instances in the ASG"
-  type = number
+  type        = number
 }
 
 variable "custom_tags" {
   description = "Custom tags to set on the Instances in the ASG"
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
 }
 
 variable "enable_autoscaling" {
   description = "If set to true, enable auto scaling"
-  type = bool
+  type        = bool
 }
 
 variable "ami" {
   description = "The AMI to run in the cluster"
-  type = string
-  default = "ami-0fb653ca2d3203ac1"
+  type        = string
+  default     = "ami-0fb653ca2d3203ac1"
 }
 
 variable "server_text" {
   description = "The text the web server should return"
-  type = string
-  default = "Hello, World"
+  type        = string
+  default     = "Hello, World"
 }
