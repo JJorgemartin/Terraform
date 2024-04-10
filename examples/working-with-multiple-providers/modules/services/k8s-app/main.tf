8108aa043bd96adf3a6bbe7933894a21c6bdc41a
jjorgemartin <jorgemartin.jjm@gmail.com>
2024-04-10 16:30:50 -0600

    add a modules for k8s

---
+++ examples/working-with-multiple-providers/modules/services/k8s-app/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "kubernetes_deployment" "app" {
+  metadata {
+    name = var.name
+  }
+  spec {
+    replicas = var.replicas
+    template {
+      metadata {
+        labels = local.pod_labels
+      }
+      spec {
+        container {
+          name  = var.name
+          image = var.image
+          port {
+            container_port = var.container_port
+          }
+          dynamic "env" {
+            for_each = var.enviroment_variables
+            content {
+              name  = env.key
+              value = env.value
+            }
+          }
+        }
+      }
+    }
+    selector {
+      match_labels = local.pod_labels
+    }
+  }
+}
+
+locals {
+  pod_labels = {
+    app = var.name
+  }
+}
+
+resource "kubernetes_service" "app" {
+  metadata {
+    name = var.name
+  }
+  spec {
+    type = "LoadBalancer"
+    port {
+      port        = 80
+      target_port = var.container_port
+      protocol    = "TCP"
+    }
+    selector = local.pod_labels
+  }
+}
