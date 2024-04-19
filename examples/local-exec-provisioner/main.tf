240757649e9910fdd9acd37eb65864715bf8cd46
jjorgemartin <jorgemartin.jjm@gmail.com>
2024-04-19 17:04:21 +0200

    add a local-exec a remote-exec examples

---
+++ examples/local-exec-provisioner/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">=1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo \"Hello, World from $(uname -smp)\""
+  }
+}
