0bfc76403aff1d96f97630e5a85f5f9c06250be2
jjorgemartin <jorgemartin.jjm@gmail.com>
2024-04-30 21:39:33 +0200

    Add a test folder

---
+++ test/alb_example_test.go
@@ -0,0 +1,12 @@
+package test
+
+import(
+	"testing"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestAlbExample(t *testing.T){
+	opts := &terraform.Options{
+		TerraformDir : ""
+	}
+}
---
+++ test/go.mod
@@ -0,0 +1,3 @@
+module github.com/jjorgemartin/Terraform
+
+go 1.22.2
