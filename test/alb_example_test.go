925da0777496b14015b3b53ae92baf039a189361
jjorgemartin <jorgemartin.jjm@gmail.com>
2024-05-01 21:44:04 +0200

    Add a some import in alb_example_test.go file

--- test/alb_example_test.go
+++ test/alb_example_test.go
@@ -1,8 +1,16 @@
 package test
 
 import(
-	"testing"
+
+	"fmt"
+	"github.com/stretchr/testify/require"
+
+	"github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"testing"
+	"time"
+
 )
 
 func TestAlbExample(t *testing.T){
